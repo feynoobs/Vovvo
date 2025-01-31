c81e3d7cbc225d6eef47a6955a521109579552d2
feynoobs <p.w.feeniej@gmail.com>
2025-02-01 00:40:22 +0900

    寝るわ

--- resources/ts/Parent.tsx
+++ resources/ts/Parent.tsx
@@ -1,28 +1,46 @@
-import React, { useCallback, useState } from "react"
+import React, { useCallback, useState, memo } from "react"
 
 type ButtonProps = {
     onClick: () => void
 }
 
-const DecrementButton = (props: ButtonProps) => {
+const DecrimentButton = memo((props: ButtonProps) => {
     const {onClick} = props
-    console.log(`DecrementButtonが再描画されました`)
-
-    return <button onClick={onClick}>Decremen</button>
-}
-
-const IncrementButton = React.memo((props: ButtonProps) => {
-    const {onClick} = props
-    console.log('IncrementButtonが再描画されました')
-    return <button onClick={onClick}>Decremen</button>
+    console.log('DecrimentButtonが再描画されました')
+    return (
+        <button onClick={onClick}>Decriment</button>
+    )
 })
 
-const DoubleButton = React.memo((props: ButtonProps) => {
+const IncrimentButton = memo((props: ButtonProps) => {
+    const {onClick} = props
+    console.log('IncrimentButtonが再描画されました')
+    return (
+        <button onClick={onClick}>Incriment</button>
+    )
+})
+
+const DoubleButton = memo((props: ButtonProps) => {
     const {onClick} = props
     console.log('DoubleButtonが再描画されました')
-    return <button onClick={onClick}>Decremen</button>
+    return (
+        <button onClick={onClick}>Double</button>
+    )
 })
+
 
 export const Parent = () => {
     const [count, setCount] = useState(0)
+    const descriment = () => {setCount((c) => c - 1)}
+    const inscriment = () => {setCount((c) => c + 1)}
+    const double = useCallback(() => {setCount((c) => c * 2)}, [])
+
+    return (
+        <div>
+            <p>Count:{count}</p>
+            <DecrimentButton onClick={descriment}></DecrimentButton>
+            <IncrimentButton onClick={inscriment}></IncrimentButton>
+            <DoubleButton onClick={double}></DoubleButton>
+        </div>
+    )
 }
