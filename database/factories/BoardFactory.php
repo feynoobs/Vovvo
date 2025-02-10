d388eb7a7aeed9de6f4cafd2c654a4c284e71e06
feynoobs <p.w.feeniej@gmail.com>
2025-02-10 22:51:53 +0900

    果心居士

---
+++ database/factories/BoardFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class BoardFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'group_id' => fake()->numberBetween(1, 4),
+            'name' => fake()->realText(10),
+            'sequence' => fake()->numberBetween(1, 10),
+        ];
+    }
+}
