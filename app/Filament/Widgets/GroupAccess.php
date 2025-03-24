921125806723a0b7502e3eb2733b5478cfcb49bb
feynoobs <p.w.feeniej@gmail.com>
2025-03-24 23:07:08 +0900

    棒グラフできた

--- app/Filament/Widgets/GroupAccess.php
+++ app/Filament/Widgets/GroupAccess.php
@@ -11,15 +11,20 @@ class GroupAccess extends ChartWidget
 
     protected function getData(): array
     {
-
-        $access = Group::withCount(['responses'])->get();
+        $groups = Group::withCount(['responses'])->orderBy('id', 'asc')->get();
         return [
-            //
+            'datasets' => [
+                [
+                    'label' => '投稿数',
+                    'data' => $groups->pluck('responses_count')->toArray(),
+                ],
+            ],
+            'labels' => $groups->pluck('name')->toArray(),
         ];
     }
 
     protected function getType(): string
     {
-        return 'line';
+        return 'bar';
     }
 }
